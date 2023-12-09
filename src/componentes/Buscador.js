5fd4408e929200320d2f3e6d28cc6105f1a12436
Jorg7Mr <102607655+Jorg7Mr@users.noreply.github.com>
2023-12-08 22:15:37 -0300

    BUSCADOR

--- src/componentes/Buscador.js
+++ src/componentes/Buscador.js
@@ -1,21 +1,23 @@
 // Buscador.js
 import React from 'react';
+import '../estilos/buscador.css'
+import iconBuscar from '../img/search.svg'
 
 const Buscador = () => {
   return (
-    <div className="row" style={{ backgroundColor: '#AEE69A' }}>
-      <div className="col-12 d-flex justify-content-center align-items-center py-3">
-        <div className="input-group">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Buscar..."
-          />
-          <div className="input-group-prepend">
-            <span
-              className="input-group-text text-white"
-              style={{ backgroundColor: '#AEE69A' }}
-            ></span>
+    <div className='contenedor-buscador'>
+      <div className="row" >
+        <div className="col-9 d-flex justify-content-center align-items-center py-2">
+
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Buscar..."
+            />
+            <div className="input-group-prepend btn-icon">
+              <span class="input-group-text" id="basic-addon1"><img src={iconBuscar} className='img-icon' /></span>
+            </div>
           </div>
         </div>
       </div>
