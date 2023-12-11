4925e92c7181ee4ea9e59e25279364a1d65b70d5
llamaz01 <144249539+llamaz01@users.noreply.github.com>
2023-12-10 23:34:01 -0300

    Update pictures.js

--- src/img/pictures.js
+++ src/img/pictures.js
@@ -26,6 +26,17 @@ export { default as tortita } from './tortita.png';
 export { default as pizza } from './pizza.png';
 export { default as budin } from './budin.png';
 
+//Recetas
+export { default as imagen1 } from './imagenReceta/imagen1.PNG';
+export { default as imagen2 } from './imagenReceta/imagen2.PNG';
+export { default as imagen3 } from './imagenReceta/imagen3.PNG';
+export { default as imagen4 } from './imagenReceta/imagen4.PNG';
+export { default as imagen5 } from './imagenReceta/imagen5.PNG';
+export { default as imagen6 } from './imagenReceta/imagen6.PNG';
+export { default as imagen7 } from './imagenReceta/imagen7.PNG';
+export { default as imagen8 } from './imagenReceta/imagen8.PNG';
+export { default as imagen9 } from './imagenReceta/imagen9.PNG';
 
+export { default as logoReceta } from './logo-recetas.png';
 
 
