5e198bb0975abb3bc70c97ed77576a78a83f85a0
Celelarramendia <103289239+Celelarramendia@users.noreply.github.com>
2024-01-22 09:45:28 -0300

    detalle final receta

--- src/componentes/RecetaDetalle.jsx
+++ src/componentes/RecetaDetalle.jsx
@@ -26,27 +26,40 @@ function RecetaDetalle() {
       {/* Mostrar toda la información de la receta en el contexto de detalle */}
       <div className="container-fluid">
         <div className="row">
-          <div className="col-12 text-center py-3" style={recuadroStyle}>
-            <h3>{receta.titulo}</h3>
+        <div className="col-12 text-center " style={recuadroStyle}>
+            <h1>{receta.titulo}</h1>
           </div>
         </div>
         <div className="row justify-content-center"> {/* Centrar la imagen */}
-          <div className="col-12 col-md-6 text-center">
-            <img src={receta.fotoSrc} alt={`Foto de ${receta.titulo}`} style={{ width: '50%' }} />
+        <div className="col-12 text-center " style={recuadroStyle1}>
+            <img src={receta.fotoSrc} alt={`Foto de ${receta.titulo}`} style={{ width: '75%' }} />
           </div>
         </div>
-        <div className="row">
-          <div className="col-12 col-md-6">
-            <div className="col-12 text-center py-3">
-              <h3>Ingredientes</h3>
+          <div className="col-12 ">
+            <div className="col-12 align-right">
+                <strong>Ingredientes</strong>
+                <ul className="ingredientes-lista">
+                  {receta.ingredientes.map((ingrediente, index) => (
+                    <li key={index} className="ingrediente">
+                      {ingrediente}
+                    </li>
+                  ))}
+                </ul>
             </div>
-            <p style={{ textAlign: 'justify' }}>{receta.ingredientes}</p>
-            <div className="col-12 text-center py-3">
-              <h3>Preparacion</h3>
+           
+            <div className="col-12 ">
+            <p>
+                <strong>Preparación</strong>
+                <ul className="preparacion-lista">
+                  {receta.preparacion.map((preparacion, index) => (
+                    <li key={index} className="preparacion">
+                      {preparacion}
+                    </li>
+                  ))}
+                </ul>
+              </p>
             </div>
-            <p style={{ textAlign: 'justify' }}>{receta.preparacion}</p>
           </div>
-        </div>
       </div>
       <Menu />
     </div>
@@ -54,15 +67,24 @@ function RecetaDetalle() {
 }
 
 ///style
+
 const recuadroStyle = {
   backgroundColor: 'rgba(174, 230, 154, 0.4)',
-  padding: '10px',
+  padding: '5%', 
+  margin: '0px',
   marginBottom: '10px',
-  height: '20%',
+  height: '100%',
   width: '100%',
   display: 'flex',
-  alignItems: 'center',
   fontSize: '1em',
+};
+const recuadroStyle1 = {
+  padding: '0%', 
+  margin: '0px',
+  marginBottom: '10px',
+  height: '60%',
+  width: '60%',
+  display: 'flex',
 };
 
 export default RecetaDetalle;
