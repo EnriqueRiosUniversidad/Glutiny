ba418d52989e0c8f2d990bd1278728c51540a33f
llamaz01 <144249539+llamaz01@users.noreply.github.com>
2024-01-25 11:27:07 -0300

    Update pictures.js

--- src/img/pictures.js
+++ src/img/pictures.js
@@ -12,7 +12,7 @@ export { default as cityMarket } from './cityMarket.png';
 
 
 export { default as logo } from './logo.png';
-export { default as logoTienda } from './logoTienda.png';
+
 
 //Tiendas Productos
 export { default as harina_de_arroz_primicia } from './harina_de_arroz_primicia.png'; 
@@ -40,5 +40,15 @@ export { default as pizza } from './pizza.png';
 export { default as budin } from './budin.png';
 
 
+//Recetas
+export { default as imagen1 } from './imagenReceta/imagen1.PNG';
+export { default as imagen2 } from './imagenReceta/imagen2.PNG';
+export { default as imagen3 } from './imagenReceta/imagen3.PNG';
+export { default as imagen4 } from './imagenReceta/imagen4.PNG';
+export { default as imagen5 } from './imagenReceta/imagen5.PNG';
+export { default as imagen6 } from './imagenReceta/imagen6.PNG';
+export { default as imagen7 } from './imagenReceta/imagen7.PNG';
+export { default as imagen8 } from './imagenReceta/imagen8.PNG';
+export { default as imagen9 } from './imagenReceta/imagen9.PNG';
 
-
+export { default as logoReceta } from './logo-recetas.png';
